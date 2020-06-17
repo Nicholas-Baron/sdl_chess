60511028dd8b20478275ea5db81fc4f3ca38eff0
Nicholas-Baron <nicholas.baron.ten@gmail.com>
2020-06-17 16:26:49 -0700

    Rendered the board at the center of the window

--- src/board.rs
+++ src/board.rs
@@ -1,8 +1,11 @@
-use chess::{Board, File, Rank, Square, NUM_FILES, NUM_RANKS, Color};
+use chess::{Board, Color, File, Rank, Square, NUM_FILES, NUM_RANKS};
 
-use sdl2::{rect::Rect, render::Renderer};
+use sdl2::{
+    rect::{Point, Rect},
+    render::Renderer,
+};
 
-use std::convert::TryInto;
+use std::convert::{TryFrom, TryInto};
 
 use crate::{sprite::Sprite, utils};
 
@@ -21,34 +24,37 @@ impl ChessBoard {
         }
     }
 
-    pub fn draw_on(&self, dest: &mut Renderer, target_area: Option<Rect>) -> Result<(), String> {
+    pub fn draw_on(&self, dest: &mut Renderer, center: Option<Point>) -> Result<(), String> {
         for x in 0..NUM_FILES {
             for y in 0..NUM_RANKS {
                 let square = Square::make_square(Rank::from_index(y), File::from_index(x));
 
                 let (x, y): (i32, _) = utils::map_tuple((x, y), |val| val.try_into().unwrap());
-                let tile_size : i32 = TILE_SIZE.into();
+                let tile_size: i32 = TILE_SIZE.into();
 
-                let rect = Rect::new(
-                    x * tile_size,
-                    y * tile_size,
-                    TILE_SIZE.into(),
-                    TILE_SIZE.into(),
-                );
+                let (mut pixel_x, mut pixel_y) = utils::map_tuple((x, y), |val| val * tile_size);
+
+                if let Some(offset) = center {
+                    let board_size: i32 = i32::try_from(NUM_FILES).unwrap() * tile_size;
+                    pixel_x += offset.x() - board_size / 2;
+                    pixel_y += offset.y() - board_size / 2;
+                }
+
+                let rect = Rect::new(pixel_x, pixel_y, TILE_SIZE.into(), TILE_SIZE.into());
 
                 if (x + y) % 2 == 0 {
-                // White square
+                    // White square
                     self.sprites[0].draw_on(dest, Some(rect))?;
                 } else {
-                // Black square
+                    // Black square
                     self.sprites[1].draw_on(dest, Some(rect))?;
                 }
 
                 if let Some(piece) = self.board.piece_on(square) {
                     let color = self.board.color_on(square).unwrap();
 
-                    use Color::*;
                     use chess::Piece::*;
+                    use Color::*;
                     match (piece, color) {
                         (Pawn, Black) => self.sprites[2].draw_on(dest, Some(rect))?,
                         (Pawn, White) => self.sprites[3].draw_on(dest, Some(rect))?,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color, render::RendererBuilder};
+use sdl2::{
+    event::Event,
+    keyboard::Keycode,
+    pixels::Color,
+    rect::{Point, Rect},
+    render::RendererBuilder,
+};
 
 use sdl2_image::{self as image, INIT_PNG};
 
@@ -23,10 +29,7 @@ fn main() {
         .build()
         .unwrap();
 
-    let mut canvas = RendererBuilder::new(window)
-        .present_vsync()
-        .build()
-        .unwrap();
+    let mut canvas = window.renderer().present_vsync().build().unwrap();
 
     let _image_context = image::init(INIT_PNG).unwrap();
 
@@ -36,13 +39,19 @@ fn main() {
     let board = ChessBoard::new(sprites);
 
     canvas.set_draw_color(Color::RGB(0, 250, 250));
+    if let Err(e) = canvas.set_logical_size(800, 600) {
+        eprintln!("{}", e);
+    }
     canvas.clear();
     canvas.present();
 
     let mut events = sdl_context.event_pump().unwrap();
+    let mut board_center = Point::from(utils::map_tuple(canvas.logical_size(), |val| {
+        use std::convert::TryFrom;
+        i32::try_from(val / 2).unwrap()
+    }));
+
     'run_loop: loop {
-        canvas.clear();
-        board.draw_on(&mut canvas, None).unwrap();
 
         while let Some(event) = events.poll_event() {
             match event {
@@ -55,7 +64,10 @@ fn main() {
             }
         }
 
+        canvas.clear();
+        board.draw_on(&mut canvas, Some(board_center)).unwrap();
         canvas.present();
+
         thread::sleep(Duration::new(0, 1_000_000_000 / 60));
     }
 
